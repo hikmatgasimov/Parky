3790bb287dba13b4fede41e3fe408d4256cb77b8
gasimovhikmat <gasimov.hikmat95@gmail.com>
2022-04-03 13:43:09 +0400

    Initial

---
+++ Parky/ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,106 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : ControllerBase
+    {
+        private INationalParkRepository _npRepo;
+        private IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _npRepo = npRepo ?? throw new ArgumentNullException(nameof(npRepo));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        [HttpGet]
+        public IActionResult GetNationalPark()
+        {
+            var objList = _npRepo.GetNationalPark();
+
+            var objDto = new List<NationalParkDto>();
+
+            foreach (var obj in objList)
+            {
+                objDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+
+            return Ok(objDto);
+        }
+        [HttpGet("{id:int}", Name = " GetNationalPark")]
+        public IActionResult GetNationalPark(int id)
+        {
+            var obj = _npRepo.GetNationalPark(id);
+
+            if (obj == null) return NotFound();
+
+            var objDto = _mapper.Map<NationalParkDto>(obj);
+
+
+            return Ok(objDto);
+        }
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null) return BadRequest(); ;
+
+            if (_npRepo.NationalParkExists(nationalParkDto.Name))
+            {
+                ModelState.AddModelError("", "National Park Exists");
+                return StatusCode(404, ModelState);
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!_npRepo.CreateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when saving the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+            return CreatedAtAction("GetNationalPark", new { nationalParkId = nationalParkObj.Id }, nationalParkObj);
+        }
+        [HttpPatch("{id:int}", Name = "UpdateNationalPark")]
+        public IActionResult UpdateNationalPark(int id,[FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null|| id != nationalParkDto.Id) return BadRequest(ModelState); ;
+        
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!_npRepo.UpdateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when updating the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();           
+        }
+        [HttpDelete("{id:int}", Name = "DeleteNationalPark")]
+        public IActionResult DeleteNationalPark(int id)
+        {     
+            if (_npRepo.NationalParkExists(id))
+            {
+                return NotFound();
+            }
+            var obj = _npRepo.GetNationalPark(id);
+
+            if (!_npRepo.DeleteNationalPark(obj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when deleting the record {obj.Name}");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
+    }
+
+}
+
---
+++ Parky/ParkyAPI/Repository/INationalParkRepository.cs
@@ -0,0 +1,20 @@
+﻿using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> GetNationalPark();
+        NationalPark GetNationalPark(int id);
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+        bool CreateNationalPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+        bool Save();
+    }
+}
